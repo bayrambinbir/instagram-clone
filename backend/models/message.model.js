6e686d8dca9e5b1c832b792cc15c76a9329d743c
bayrambinbir <38406467+bayrambinbir@users.noreply.github.com>
2025-08-09 16:54:18 -0500

    create comment and conversation models

---
+++ backend/models/message.model.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const messageSchema = mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  message: { type: String, required: true },
+});
+
+export default Message = mongoose.model("Message", messageSchema);
