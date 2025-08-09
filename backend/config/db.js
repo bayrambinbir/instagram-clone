084f093a97e8950e4cdb44954ab4ea551f4848b1
bayrambinbir <38406467+bayrambinbir@users.noreply.github.com>
2025-08-09 08:33:59 -0500

    create user model

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
+let cashedDBConnection = null;
 
 const connectDB = async () => {
   try {
+    if (cashedDBConnection) {
+      return cashedDBConnection;
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected to "${conn.connection.name}" app`);
+    cashedDBConnection = conn;
     return conn;
   } catch (error) {
     console.error("MongoDB connection failed:", error.message);
--- backend/index.js
+++ backend/index.js
@@ -37,6 +37,7 @@ app.get("/", (req, res) => {
 const listen = async () => {
   const conn = await connectDB();
   if (conn) {
+    connectDB();
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
