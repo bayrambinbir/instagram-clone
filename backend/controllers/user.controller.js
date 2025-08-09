e0edcd8740f73a850001cd9ca3866cbdbf605543
bayrambinbir <38406467+bayrambinbir@users.noreply.github.com>
2025-08-09 17:18:28 -0500

    create user controller

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+//Registration Logic
+
+export const register = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+      return res.status(400).json({
+        message: "All fields are required",
+        success: false,
+      });
+    }
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({
+        message: "User already exists",
+        success: false,
+      });
+    }
+    // Hash the password before saving
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Create new user
+    await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    return res.status(201).json({
+      message: "User registered successfully",
+      success: true,
+    });
+  } catch (error) {}
+};
